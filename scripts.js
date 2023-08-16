3ed51e82cf57548c99ba90142c5331b18d72143d
annorahlewis <102407979+annorahlewis@users.noreply.github.com>
2023-08-16 15:58:40 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Toggle burger menu
+const burgerMenu = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+burgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+// Other JavaScript functionality can be added here
